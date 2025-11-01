322814766bfe0d99532747615b7c5419fdca697a
Obed704 <ericgob7@gmail.com>
2025-11-01 13:19:09 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // ✅ must be /
+  base: "/", // ✅ important for production
   build: {
     outDir: "dist",
   },
