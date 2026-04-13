1728121420be264c67504dee3024ea336a7539f1
niyobyose obed <niyobyoseobed1@gmail.com>
2026-04-13 15:36:07 +0200

    fix: update image import for production

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // must be absolute
+  base: "/ourProjects/", // must be absolute
   build: {
     outDir: "dist", // default Vite output
   },
